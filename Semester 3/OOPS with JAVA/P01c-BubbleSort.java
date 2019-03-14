2b2692ea688de86964a22e33600e3c2703cb9a53
Mahima-M-A <43963605+Mahima-M-A@users.noreply.github.com>
2019-03-14 19:01:07 +0530

    Create P01c-BubbleSort.java

---
+++ Semester 3/OOPS with JAVA/P01c-BubbleSort.java
@@ -0,0 +1,28 @@
+//Program to implement bubble sort 
+
+import java.util.Scanner;
+class BubbleSort
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the array size:");
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        
+        System.out.println("Enter "+n+" numbers:");
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        for(int i=0;i<n-1;i++)
+            for(int j=0;j<n-i-1;j++)
+                if(a[j]>a[j+1])
+                {
+                    int temp=a[j+1];
+                    a[j+1]=a[j];
+                    a[j]=temp;
+                }
+        System.out.println("Sorted array:");
+        for(int i=0;i<n;i++)
+            System.out.println(a[i]);
+    }
+}
