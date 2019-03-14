42646e47d25d5ee1b11c1f859adfb8b7310624cf
Mahima-M-A <43963605+Mahima-M-A@users.noreply.github.com>
2019-03-14 19:20:46 +0530

    Create MainClass.java

---
+++ Semester 3/OOPS with JAVA/P10/MainClass.java
@@ -0,0 +1,10 @@
+import ISE.*;
+class MainClass
+{
+    public static void main(String[] args)
+    {
+        ISE_department ob=new ISE_department();
+        ob.readdata();
+        ob.printdata();
+    }
+}
