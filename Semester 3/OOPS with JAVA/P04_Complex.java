678e13b4d80b89e29e7e0e56644222290b81a8cc
Mahima-M-A <43963605+Mahima-M-A@users.noreply.github.com>
2019-03-14 19:38:18 +0530

    Update P04_Complex.java

--- Semester 3/OOPS with JAVA/P04_Complex.java
+++ Semester 3/OOPS with JAVA/P04_Complex.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 class P04_Complex
 {
     float real1,img1,real2,img2,ans_r,ans_i;
-    Complex()
+    P04_Complex()
     {
         ans_r=0;
         ans_i=0;
     }
-    Complex(float real1,float img1,float real2,float img2) //to initialize the complex numbers
+    P04_Complex(float real1,float img1,float real2,float img2) //to initialize the complex numbers
     {
         this.real1=real1;
         this.real2=real2;
@@ -37,14 +37,14 @@ class P04_Complex
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        Complex ob=new Complex();
+        P04_Complex ob=newP04_ Complex();
         System.out.println("Enter the real and imaginary parts of first complex number:");
         ob.real1=sc.nextFloat();
         ob.img1=sc.nextFloat();
         System.out.println("Enter the real and imaginary parts of second complex number:");
         ob.real2=sc.nextFloat();
         ob.img2=sc.nextFloat();
-        Complex ob1=new Complex(ob.real1,ob.img1,ob.real2,ob.img2);
+        P04_Complex ob1=new P04_Complex(ob.real1,ob.img1,ob.real2,ob.img2);
         ob1.add();
         ob1.subtract();
         ob1.multiply();
