732e05bbe2e64d1025225cc34ef59bb126a18a8f
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-03-27 20:41:02 +0530

    Create P04a-Palindrome.asm

---
+++ Semester 4/MIcroprocessor Lab/P04a-Palindrome.asm
@@ -0,0 +1,41 @@
+assume cs:code,ds:data
+data segment
+    str1 db "madam" ;string to be checked for
+    n db n-str1 ;contains size of str1
+    str2 db 5 dup(?) ;to copy the reverse of str2
+    msg1 db "It is a Palindrome$"
+    msg2 db "It is not a Palindrome$"
+data ends
+code segment
+    start:
+        mov ax,data
+        mov ds,ax
+        mov es,ax 
+        mov cx,n ;contains str1 size
+        lea si,n
+        dec si ;points to the position of last char in str1
+        lea di,str2 ;points to the first char position of str2 
+    next:
+        mov al,[si] ;to copy
+        mov [di],al ;the reverse of str1 into str2
+        inc di
+        dec si
+        loop next ;loop until str1 is completely copied into str2
+        
+        lea si,str1 ;points to the first char position of str1
+        lea di,str2 ;points to the first char position of str2
+        mov cx,n ;contains str1 size
+        cld ;to clear direction
+        rep cmpsb ;compare str1 and str2 char by char
+        jnz failure ;jump if unequal
+        lea dx,msg1 ;load msg1
+        jmp display ;jump to display
+    failure:
+        lea dx,msg2 ;load msg2
+    display:
+        mov ah,09h ;to print string
+        int 21h
+        mov ah,07h ;to redirect char from stdin
+        int 21h
+code ends
+end start
