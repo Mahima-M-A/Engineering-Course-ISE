31c9457ee95c51119d63cad20e0f08bb7e6c329d
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-05-26 19:23:52 +0530

    Update P02aBubbleSort.asm

--- Semester 4/Microprocessor Lab/P02aBubbleSort.asm
+++ Semester 4/Microprocessor Lab/P02aBubbleSort.asm
@@ -11,24 +11,20 @@ code segment
         dec bx  ;contains the last array element's position
     nextpass:
         mov cx,bx ;copies the last array element's position
-        mov ah,00 ;set ah to 0
         lea si,arr ;loads the first array elemnt's position into si
     nextloop:
         mov al,[si] ;contains the array element pointed to by si
         cmp al,[si+1] ;compares the array element pointed to by si with the next element
-        jle do_nothing ;jump if the array element pointed to by si is less than or equal to the nezt element
+        jbe do_nothing ;jump if the array element pointed to by si is less than or equal to the nezt element
         xchg al,[si+1] ;else swap
         mov [si],al ;copy al's value to the array position pointed to by si
-        mov ah,1 ;set ah to 1
     do_nothing:
         inc si ;increment si to point to next element
         loop nextloop ;continue looping
-        cmp ah,0 ;check if ah is 0
-        je finish ;then jump to finish
         dec bx ;else decrement bx
         jnz nextpass ;continue nextpass
-    finish:
-        mov ah,4ch 
-        int 21h ;to exit program
+
+        mov ah,4ch ;to exit program 
+        int 21h
 code ends
 end start
