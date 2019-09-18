06632ace2c1c8c14c19d2d4ff538bd647ce72604
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-09-18 20:05:23 +0530

    Update Sentence.py

--- Semester 5/Scripting Languages Lab/Python Programs/Sentence.py
+++ Semester 5/Scripting Languages Lab/Python Programs/Sentence.py
@@ -8,15 +8,20 @@ class Reverse:
 		for str in w:
 			print(str,end=" ")
 		print()
+
 c=0
-n=0
 s=[]
 
+#to input 3 sentences
 while c<3:
 	sen=input("Enter a sentence: ")
-	s.append(sen)
+	s.append(sen) #to create a list of the entered sentences
 	c+=1
+
+#to sort the entered sentences in descending order based on the number of vowels in each of them 
 s1=sorted(s,key=lambda word: sum(ch in 'AEIOUaeiou' for ch in word),reverse=True)
+
+#to reverse the words in each of the sentences
 for sen in s1:
-    ob=Reverse(sen)
-    ob.rev()
+    ob=Reverse(sen) #to create an instance class Reverse
+    ob.rev() #to call the rev() present in the class
