1464a7cdae74b594ba8f36fe961e3717be9d467b
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-11-21 22:13:27 +0530

    Create client.c

---
+++ Semester 5/CN Lab/1a/client.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include <arpa/inet.h>
+
+#define bufsize 1024
+
+int main(){
+
+  int clientSocket;
+  char buffer[1024],fname[255];
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+
+  /*---- Create the socket. The three arguments are: ----*/
+  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  
+  /*---- Configure settings of the server address struct ----*/
+  /* Address family = Internet */
+  serverAddr.sin_family = AF_INET;
+  /* Set port number, using htons function to use proper byte order */
+  serverAddr.sin_port = htons(7891);
+  /* Set IP address to localhost */
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  /* Set all bits of the padding field to 0 */
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  /*---- Connect the socket to the server using the address struct ----*/
+  addr_size = sizeof serverAddr;
+  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+
+  printf("\nEnter filename: ");scanf("%s",fname);
+
+  send(clientSocket,fname,255,0);
+  printf("\nResponse:\n");
+  /*---- Read the message from the server into the buffer ----*/
+  while((recv(clientSocket, buffer, bufsize, 0))>0)
+    printf("%s",buffer);
+  printf("\n");
+   
+
+  return close(clientSocket);
+}
