ff294c23809036313057f9fdca3dee3ebe3f8e1c
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-12-01 12:20:17 +0530

    Create P8.sql

---
+++ Semester 5/DBMS Lab/PL-SQL/P8.sql
@@ -0,0 +1,12 @@
+-- Program to raise a trigger when EMPLOYEE table is modified on a particular day(here, saturday or sunday)
+
+set serveroutput on;
+
+create or replace trigger myTrigger3
+before insert or delete or update on customer
+begin
+	if to_char(sysdate,'Dy') in ('Sat','Sun') then
+		raise_application_error(-20520,'Not allowed');
+	end if;
+end;
+/
